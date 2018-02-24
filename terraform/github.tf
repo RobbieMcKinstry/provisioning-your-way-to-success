0f34efa945469a5bb0268c2d386fcfb7b6407cd7
Robbie McKinstry <robbie@hashicorp.com>
2018-02-24 03:39:47 -0500

    update gh config

--- terraform/github.tf
+++ terraform/github.tf
@@ -1,5 +1,5 @@
 provider "github" {
-  token = "efae2c63cb9c7cbf063df60162043e0b26cda3b7"
+  # token = "efae2c63cb9c7cbf063df60162043e0b26cda3b7"
   organization = "CentralCatholic"
 }
 
@@ -10,8 +10,8 @@ resource "github_repository" "steelhacks-repo" {
 }
 
 resource "github_repository_collaborator" "DanJustice" {
-  repository = "OurSteelhacksRepo"
-  username   = "danieljustice"
-  permission = "admin"
-  depends_on = ["github_repository.steelhacks-repo"]
+   repository = "${github_repository.steelhacks-repo.name}"
+   depends_on = ["github_repository.steelhacks-repo"]
+   username   = "danieljustice"
+   permission = "admin"
 }

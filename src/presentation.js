b3c505e2919a8874e9ceb5f5f4840a5144ae0ed7
Robbie McKinstry <robbie@hashicorp.com>
2018-02-24 03:20:40 -0500

    update presentation content

--- src/presentation.js
+++ src/presentation.js
@@ -3,13 +3,10 @@ import React from 'react';
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
+  Code,
   Deck,
   Heading,
-  ListItem,
-  List,
-  Quote,
+  Image,
   Slide,
   Text,
 } from 'spectacle';
@@ -41,54 +38,132 @@ export default class Presentation extends React.Component {
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
+        <Slide transition={['zoom']} bgColor="tertiary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            Provisioning Your Way to a Successful Hack
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+        </Slide>
+        
+        {/* About me */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+            <Text size={2} textColor="secondary">Robbie McKinstry</Text>
+            <Text size={2} textColor="secondary">HashiCorp</Text>
+            <Text size={2} textColor="secondary">robbie@hashicorp.com</Text>
+            <Text size={2} textColor="secondary">Pitt CS Class of 2017</Text>
+        </Slide>
+
+        {/* Define Provisioning */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            define pro·vi·sion :
+          </Heading>
+            <Text size={2} textColor="secondary">
+                the act of obtaining or preparing resources prior to use
+            </Text>
+        </Slide>
+
+        {/* Simplest Case */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            Simplest case:
+          </Heading>
+        </Slide>
+
+        {/* When you ship your app */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            When you ship your app
+          </Heading>
+        </Slide>
+
+        {/* Define Provisioning */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            define ship :
+          </Heading>
+          <Text size={2} textColor="secondary">
+             the act of deploying your app to a public, live machine 
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
+ 
+        {/* But we'll get to that */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            Hold your horses
           </Heading>
+        </Slide>
+ 
+        {/* Lets do something simple first */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            First things first
+          </Heading>
+        </Slide>
+
+        {/* Let's Set up a repository:
+            This is the kind of thing you'd do right at the start of a hackathon
+        */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            <Code lang={"bash"}>git init</Code>
+          </Heading>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            define re·pos·i·to·ry 
+          </Heading>
+          <Text size={2} textColor="secondary">
+             a remote storage volume for saving your source code, usually also saving the history of all changes to the source code
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={1} textColor="secondary">
-            Heading 1
+            <iframe title={"mailtime"} src="https://giphy.com/embed/aGCDmZpfiVhPG" width="1080" height="608" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
           </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary">
+            <iframe title="smash" src="https://giphy.com/embed/xUOwG40nnyMTra0Xte" width="1080" height="608" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
           </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+            <Image src="github.jpg" fit fill />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="secondary" caps>
+            Automation!
           </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary" caps>
+            Why automate what could be UI?
           </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
+        </Slide>
+
+        {/* Useful when projects scale, namely in people */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={2} textColor="secondary" caps>
+            Scale
           </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
+         </Slide>
+
+        {/* Organization Scale is how well you deal with your teams/departments/organization 
+            changing size 
+        */}
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={1} textColor="primary">
+            define or·gan·i·za·tion·al scale
+          </Heading>
+          <Text size={2} textColor="secondary">
+             how well your organization deals with teams/departments/organization changing size
           </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
+         </Slide>
       </Deck>
     );
   }
